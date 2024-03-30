4995eb78255b971a724e48285cf6e3ce63c9c7b2
bertrandong <bertrandongchangheng@gmail.com>
2024-03-31 00:45:28 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/AddMenuCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddMenuCommandParser.java
@@ -7,7 +7,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_PRODUCT_NAME;
 import java.util.stream.Stream;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.AddMenuCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.order.Product;
