bcb8baf1924de261f6ec4f519ef49f8a570c2845
bertrandong <bertrandongchangheng@gmail.com>
2024-03-31 02:44:06 +0800

    Delete extra method in DeleteCommandParser

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -21,15 +21,6 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
     public DeleteCommand parse(String args) throws ParseException {
         requireNonNull(args);
 
-        // See if still needed
-        //try {
-        //    Index index = ParserUtil.parseIndex(args);
-        //    return new DeleteCommand(index);
-        //} catch (ParseException pe) {
-        //    throw new ParseException(
-        //            String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
-        //}
-
         if (args.startsWith(" " + PREFIX_CUSTOMER_ID.toString())) {
             DeleteCustomerCommandParser deleteCustomerCommandParser = new DeleteCustomerCommandParser();
             return deleteCustomerCommandParser.parse(args);
