5a3f7c8ae924ae2c819b52cc23729a2f8fde3f49
Joelwang22 <omegabeeminecraft@gmail.com>
2024-04-03 03:59:58 +0800

    Fix Style

--- src/test/java/seedu/address/model/order/CreationDateTest.java
+++ src/test/java/seedu/address/model/order/CreationDateTest.java
@@ -1,9 +1,14 @@
 package seedu.address.model.order;
 
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalCreationDate.APRIL_FOOLS;
 import static seedu.address.testutil.TypicalCreationDate.NEW_YEARS;
+
+import org.junit.jupiter.api.Test;
+
 
 public class CreationDateTest {
 
