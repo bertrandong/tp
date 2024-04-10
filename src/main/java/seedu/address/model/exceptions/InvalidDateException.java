c8163d6cd63129101a693e9152e2699ee53ec2cb
bertrandong <bertrandongchangheng@gmail.com>
2024-04-10 13:16:42 +0800

    Fix edit menu item error message

--- src/main/java/seedu/address/model/exceptions/InvalidDateException.java
+++
@@ -1,7 +0,0 @@
-package seedu.address.model.exceptions;
-
-/**
- * Signals when an invalid date is used.
- */
-public class InvalidDateException extends IllegalArgumentException{
-}
--- src/main/java/seedu/address/model/exceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package seedu.address.model.exceptions;
-
-/**
- * Signals that the operation is unable to find the specified product.
- */
-public class ProductNotFoundException extends RuntimeException {
-}
